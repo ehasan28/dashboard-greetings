7946c377b4ed8b9acc850fa1865bca13deb64c87
Ehasanul Haque <ehasan.best@gmail.com>
2026-02-03 12:03:21 +0600

    added new quotes

--- dashboard-greetings.php
+++ dashboard-greetings.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:       Dashboard Greetings
  * Plugin URI:        https://wordpress.org/plugins/dashboard-greetings
@@ -14,11 +13,10 @@
  * Text Domain:       dashboard-greetings
  * Domain Path:       /languages
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
- * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
  */
-
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
